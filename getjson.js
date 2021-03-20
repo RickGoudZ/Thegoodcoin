75cf45daf8b617709d168b813ba0f6b7b71849f2
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 14:05:06 +0100

    Update getjson.js

--- getjson.js
+++ getjson.js
@@ -1,20 +1,11 @@
 // JavaScript Document
 var staticUrl = 'https://www.thegoodcoin.nl/test.json';
 
-$.ajax({
-    url: 'https://www.thegoodcoin.nl/test.json',
-
-    dataType: "jsonp",
-    success: function(response) {
-        console.log(response); // server response
-    }
-
-});
-/*$.getJSON(staticUrl, function(data){
+$.getJSON(staticUrl, function(data){
 	console.log(data)
-});*/
+});
 
 function fonctiont() {	
 		var output = document.getElementById('jsont');
-		output.innerHTML = response;
+		output.innerHTML = 'new content';
 }
