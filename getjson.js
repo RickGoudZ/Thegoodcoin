54bc124b33a8613d1032e2c3962b95b89ca32376
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 14:49:50 +0100

    Update getjson.js

--- getjson.js
+++ getjson.js
@@ -2,11 +2,12 @@
 var staticUrl = 'https://jsonplaceholder.typicode.com/users';
 
 var nissan = $.getJSON(staticUrl, function(data){
-				console.log(data)
-				console.log(nissan)
+				console.log(data.name)
+				var output = document.getElementById('jsont');
+				output.innerHTML = data.email;
 });
 
 function fonctiont() {	
-		var output = document.getElementById('jsont');
+		var output = document.getElementById('jsontd');
 		output.innerHTML = 'new content';
 }
