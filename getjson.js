67490a6213afa55718c73833ac7bbb3e9774d2c4
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 23:03:13 +0100

    Update getjson.js

--- getjson.js
+++ getjson.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 $.getJSON('test.json', function(data){
-				console.log(data.z[0.date])
+				console.log(data.z[0].date])
 				console.log(typeof data)
 				//data = JSON.stringify(data)
 				//data = Object.values(data);
