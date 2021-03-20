c1216b5b4873c38ffe7af4203a2f4740ad3b27d5
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 15:00:49 +0100

    Update getjson.js

--- getjson.js
+++ getjson.js
@@ -3,8 +3,9 @@ var staticUrl = 'https://jsonplaceholder.typicode.com/users';
 
 var nissan = $.getJSON(staticUrl, function(data){
 				console.log(data)
-				data = KSON.stringify(data)
-				console.log(data)
+				//data = JSON.stringify(data)
+				data = JSON.stringify(data)
+				console.log(data.id)
 				var output = document.getElementById('jsont');
-				output.innerHTML = data;
+				output.innerHTML = data.id;
 });
