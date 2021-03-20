d8aaf34bfcf5d4665681522c8f6889dd8388e350
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 22:40:19 +0100

    local

--- getjson.js
+++ getjson.js
@@ -1,26 +1,15 @@
 // JavaScript Document
-var staticUrl = 'https://thegoodcoin.nl/test.json'
+var staticUrl = 'https://jsonplaceholder.typicode.com/users'
 
-/*var nissan = $.getJSON(staticUrl, function(data){
-				output = jsonp&callback=?
+$.getJSON('test.json', function(data){
 				console.log(data)
-				console.log(output)
-				data = JSON.stringify(data)
+				//data = JSON.stringify(data)
 				//data = Object.values(data);
 
 				//var output = document.getElementById('jsont');
 				//output.innerHTML = data[1];
-				document.getElementById('jsont').innerHTML = data;
-});*/
-
-$.ajax({
-    url: staticUrl,
-    dataType: 'JSONP',
-    jsonpCallback: 'callback',
-    type: 'GET',
-    success: function (data) {
-        console.log(data);
-		document.getElementById('jsont').innerHTML = data;
-
-    }
+				//document.getElementById('jsont').innerHTML = data;
+				for (i = 0; i < data.length; i = i + 1){
+					console.log(data[i].name)
+				}
 });
