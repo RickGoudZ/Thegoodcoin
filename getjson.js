3672fcf7a6402537e1c003c7c5d124ad407925bf
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 22:47:35 +0100

    u

--- getjson.js
+++ getjson.js
@@ -3,13 +3,11 @@ var staticUrl = 'https://jsonplaceholder.typicode.com/users'
 
 $.getJSON('test.json', function(data){
 				console.log(data)
+				console.log(typeof data)
 				//data = JSON.stringify(data)
 				//data = Object.values(data);
 
 				//var output = document.getElementById('jsont');
 				//output.innerHTML = data[1];
-				//document.getElementById('jsont').innerHTML = data;
-				for (i = 0; i < data.length; i = i + 1){
-					console.log(data[i].name)
-				}
-});
+				document.getElementById('jsont').innerHTML = data;
+				});
