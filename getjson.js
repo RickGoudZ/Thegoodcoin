4fe43cbd010a62c160897c2d6e9976945a516252
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-20 18:39:16 +0100

    u

--- getjson.js
+++ getjson.js
@@ -3,9 +3,13 @@ var staticUrl = 'https://jsonplaceholder.typicode.com/users';
 
 var nissan = $.getJSON(staticUrl, function(data){
 				console.log(data)
-				//data = JSON.stringify(data)
 				data = JSON.stringify(data)
-				console.log(data.id)
-				var output = document.getElementById('jsont');
-				output.innerHTML = data.id;
+				//data = Object.values(data);
+
+				//var output = document.getElementById('jsont');
+				//output.innerHTML = data[1];
+				document.getElementById('jsont').innerHTML = data[1];
 });
+"350z":[
+	{"date":"03-12-2021", "price":"16340"},
+	{"date":"04-12-2021", "price":"16230"}
