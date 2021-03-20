765c0cd36308615e9104f6de05a3ae884e896d19
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-21 00:36:21 +0100

    working

--- getjson.js
+++ getjson.js
@@ -1,11 +1,14 @@
 // JavaScript Document
 $.getJSON('test.json', function(data){
-				console.log(data)
+				console.log(data[0].Nissanzp)
 				console.log(typeof data)
 				//data = JSON.stringify(data)
 				//data = Object.values(data);
 
 				//var output = document.getElementById('jsont');
 				//output.innerHTML = data[1];
-				document.getElementById('jsont').innerHTML = data;
+				var popNz = data[0].Nissanzp.pop();
+				document.getElementById('LastAvrNz').innerHTML = popNz;
+				var popAr = data[0].Audirp.pop();
+				document.getElementById('LastAvrAr').innerHTML = popAr;
 				});
