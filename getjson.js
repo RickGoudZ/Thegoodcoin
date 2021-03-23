c225ded54efaa7abbf68200aa935046c16c63659
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-23 13:16:18 +0100

    u

--- getjson.js
+++ getjson.js
@@ -13,20 +13,3 @@ $.getJSON('test.json', function(data){
 				document.getElementById('LastAvrAr').innerHTML = popAr;
 				});
 
-(function() {
-    function scrollHorizontally(e) {
-        e = window.event || e;
-        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        document.getElementById('carrow').scrollLeft -= (delta * 40); // Multiplied by 40
-        e.preventDefault();
-    }
-    if (document.getElementById('carrow').addEventListener) {
-        // IE9, Chrome, Safari, Opera
-        document.getElementById('carrow').addEventListener('mousewheel', scrollHorizontally, false);
-        // Firefox
-        document.getElementById('carrow').addEventListener('DOMMouseScroll', scrollHorizontally, false);
-    } else {
-        // IE 6/7/8
-        document.getElementById('carrow').attachEvent('onmousewheel', scrollHorizontally);
-    }
-})();
