ba7075498958c94ca7709119636e21c55a69ace9
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-24 12:13:15 +0100

    Update getcsv.js

--- getcsv.js
+++ getcsv.js
@@ -4,8 +4,8 @@ $.ajax({
   url: "data.csv",
   dataType: "text",       
   success: function(response) {
-	console.log(response)
+	console.log(typeof response)
 	//data = $.csv.toArrays(response);
-	//var csvt = document.getElementById('csvdata').innerHTML = data;
+	document.getElementById('csvdata').innerHTML = response;
   }   
 });
