1ff6c50ea98c8c8c7a40fbc8b72616186f85d7da
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-27 18:23:32 +0100

    test

--- getjson.js
+++ getjson.js
@@ -7,8 +7,12 @@ $.getJSON('dataJSON.json', function(data){
 
 				//var output = document.getElementById('jsont');
 				//output.innerHTML = data[0].date;
+				//#################
 				var popNz = data[0].Nissan350z.pop(); //pop = last from array.
 				document.getElementById('LastAvrNz').innerHTML = popNz;
+				var dif350z = data[0].Nissan350z.slice(-2, -1)[0];
+				document.getElementById('test').innerHTML = dif350z;
+				//#################
 				var popAr = data[0].Audir8.pop();
 				document.getElementById('LastAvrAr').innerHTML = popAr;
 				var popP9 = data[0].Porsche944.pop();
