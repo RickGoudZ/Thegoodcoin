1f70f01305b630e048c8f0cdd40e3a6db8175096
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-28 18:52:05 +0200

    color fix

--- getjson.js
+++ getjson.js
@@ -22,12 +22,10 @@ $.getJSON('dataJSON.json', function(data){
 				var increace350zPercentage = (increace350z / originalNb350z) * 100
 				var rounded350z = Math.round(increace350zPercentage * 10) / 10
 				document.getElementById('dif350z').innerHTML = (increace350zPercentage<0?"":"+") + rounded350z + "%";
-				if (rounded350z = 0) {
-					dif350z.style.color = "white";
-				} else if (rounded350z >= 0) {
-					dif350z.style.color = "rgb(223, 79, 54)";//red
-				} else {
+				if (rounded350z >= 0) {
 					dif350z.style.color = "rgb(75, 212, 75)";//green
+				} else {
+					dif350z.style.color = "rgb(223, 79, 54)";//red
 				}
 				//########R8##############
 				var originalNbr8 = data[0].Audir8.slice(-1);
@@ -35,9 +33,7 @@ $.getJSON('dataJSON.json', function(data){
 				var increacer8Percentage = (increacer8 / originalNbr8) * 100
 				var roundedr8 = Math.round(increacer8Percentage * 10) / 10
 				document.getElementById('difr8').innerHTML = (increacer8Percentage<0?"":"+") + roundedr8 + "%";
-				if (roundedr8 = 0) {
-					difr8.style.color = "white";
-				} else if (roundedr8 >= 0) {
+				if (roundedr8 >= 0) {
 					difr8.style.color = "rgb(223, 79, 54)";//red
 				} else {
 					difr8.style.color = "rgb(75, 212, 75)";//green
@@ -48,9 +44,7 @@ $.getJSON('dataJSON.json', function(data){
 				var increace944Percentage = (increace944 / originalNb944) * 100
 				var rounded944 = Math.round(increace944Percentage * 10) / 10
 				document.getElementById('dif944').innerHTML = (increace944Percentage<0?"":"+") + rounded944 + "%";
-				if (rounded944 = 0) {
-					dif944.style.color = "white";
-				} else if (rounded944 > 0) {
+				if (rounded944 >= 0) {
 					dif944.style.color = "rgb(223, 79, 54)";//red
 				} else {
 					dif944.style.color = "rgb(75, 212, 75)";//green
