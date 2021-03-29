34461fb7a6abed9007956fb3d04492c4ddc3e6d1
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-03-29 20:18:50 +0200

    color fix

--- getjson.js
+++ getjson.js
@@ -23,9 +23,9 @@ $.getJSON('dataJSON.json', function(data){
 				var rounded350z = Math.round(increace350zPercentage * 10) / 10
 				document.getElementById('dif350z').innerHTML = (increace350zPercentage<0?"":"+") + rounded350z + "%";
 				if (rounded350z >= 0) {
-					dif350z.style.color = "rgb(75, 212, 75)";//green
-				} else {
 					dif350z.style.color = "rgb(223, 79, 54)";//red
+				} else {
+					dif350z.style.color = "rgb(75, 212, 75)";//green
 				}
 				//########R8##############
 				var originalNbr8 = data[0].Audir8.slice(-1);
