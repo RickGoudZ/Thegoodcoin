8f9a237c3acf2ca75599fd9ba13837f552d8d8e1
RickGoudZ <80783102+RickGoudZ@users.noreply.github.com>
2021-06-19 23:01:15 +0200

    new project

--- getjson.js
+++ getjson.js
@@ -1,23 +1,36 @@
-// JavaScript Document
-$.getJSON('dataJSON.json', function(data){
-				console.log(data[0].Nissan350z)
-				console.log(typeof data)
+// JavaScript Document  https://api.jsonbin.io/v3/b/608c63dbd64cd16802a583c4
+//let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+  if (req.readyState == XMLHttpRequest.DONE) {
+    console.log(req.responseText);
+	var data = JSON.parse(req.responseText);
+	console.log(data);
+				console.log(data['record'][0].Nissan350z)
+				//console.log(typeof data)
 				//data = JSON.stringify(data)
 				//data = Object.values(data);
 
 				//var output = document.getElementById('jsont');
 				//output.innerHTML = data[0].date;
 				//#################
-				var popNz = data[0].Nissan350z.pop(); //pop = last from array.
+				let counter = 0;
+				for (let i = 0; i < data['record'][0]["date"].length; i++) {
+					counter++;
+				}
+				//console.log(data['record'][0]['Nissan350z'])
+				document.getElementById('counter').innerHTML = counter;
+				//#################
+				var popNz = data['record'][0].Nissan350z.pop(); //pop = last from array.
 				document.getElementById('LastAvrNz').innerHTML = popNz;
 				//#################
-				var popAr = data[0].Audir8.pop();
+				var popAr = data['record'][0].Audir8.pop();
 				document.getElementById('LastAvrAr').innerHTML = popAr;
 				//#################
-				var popP9 = data[0].Porsche944.pop();
+				var popP9 = data['record'][0].Porsche944.pop();
 				document.getElementById('LastAvrP9').innerHTML = popP9;
 				//#######350z###########
-				var originalNb350z = data[0].Nissan350z.slice(-1);
+				var originalNb350z = data['record'][0].Nissan350z.slice(-1);
 				var increace350z = (popNz - originalNb350z)
 				var increace350zPercentage = (increace350z / originalNb350z) * 100
 				var rounded350z = Math.round(increace350zPercentage * 10) / 10
@@ -30,7 +43,7 @@ $.getJSON('dataJSON.json', function(data){
 					dif350z.style.color = "rgb(75, 212, 75)";//green
 				}
 				//########R8##############
-				var originalNbr8 = data[0].Audir8.slice(-1);
+				var originalNbr8 = data['record'][0].Audir8.slice(-1);
 				var increacer8 = (popAr - originalNbr8)
 				var increacer8Percentage = (increacer8 / originalNbr8) * 100
 				var roundedr8 = Math.round(increacer8Percentage * 10) / 10
@@ -43,7 +56,7 @@ $.getJSON('dataJSON.json', function(data){
 					difr8.style.color = "rgb(75, 212, 75)";//green
 				}
 				//#######944##############
-				var originalNb944 = data[0].Porsche944.slice(-1);
+				var originalNb944 = data['record'][0].Porsche944.slice(-1);
 				var increace944 = (popP9 - originalNb944)
 				var increace944Percentage = (increace944 / originalNb944) * 100
 				var rounded944 = Math.round(increace944Percentage * 10) / 10
@@ -55,4 +68,9 @@ $.getJSON('dataJSON.json', function(data){
 				} else {
 					dif944.style.color = "rgb(75, 212, 75)";//green
 				}
-				});
+  }
+};
+
+req.open("GET", "https://api.jsonbin.io/v3/b/608c63dbd64cd16802a583c4", true);
+req.setRequestHeader("X-Master-Key", "$2b$10$hGmNQIDEGHH12i4HCPqSY.VdijWoHZpQ83lnsnNG2QTB4heM/HMZO");
+req.send();
